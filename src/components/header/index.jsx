d97779aa9b7153b707a85ce51b1d4f8bd8cd35ac
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-06-03 19:09:39 -0300

    searchedVakue state created

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,3 +1,4 @@
+import react, { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 
 import {
@@ -8,17 +9,22 @@ import {
     HeaderSearchButton
 } from './styles';
 
-const Header = () => (
-    <HeaderSection>
+const Header = () => {
+    const [searchedValue, setSearchedValue] = useState('');
+
+
+    return (
+     <HeaderSection>
         <HeaderTitle>Github Profile</HeaderTitle>
         <HeaderInputContainer>
-            <HeaderInput />
+            <HeaderInput value={searchedValue} onChange={(e) => setSearchedValue(e.target.value)}/>
             
             <HeaderSearchButton>
                 <FiSearch size={15} />
             </HeaderSearchButton>
         </HeaderInputContainer>
     </HeaderSection>
-);
+)
+};
 
 export default Header;
