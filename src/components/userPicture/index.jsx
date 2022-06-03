83c569d59c66682854835106847753a734e635cb
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-06-03 15:56:10 -0300

    User picture and Details Programmed

---
+++ src/components/userPicture/index.jsx
@@ -0,0 +1,9 @@
+import { Container, ProfilePicture } from './styles';
+
+const UserPicture = props => (
+    <Container>
+        <ProfilePicture src={props?.url} alt={props?.alternativeText}></ProfilePicture>
+    </Container>
+);
+
+export default UserPicture;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,16 @@
 import Container from '../components/container';
 import Header from '../components/header';
 import UserContainer from '../components/userContainer';
+import UserDetails from '../components/userDetails';
+import UserPicture from '../components/userPicture';
 
 export default function Home() {
     return (
         <Container>
             <Header />
             <UserContainer>
-                
+                <UserPicture url="https://avatars.githubusercontent.com/u/52041311?v=4" alternativeText="image" />
+                <UserDetails name="Marcos Ferreira" login="#absolutetrue" bio="Texto sim Verdadeiro"/>
             </UserContainer>
         </Container>
     );
